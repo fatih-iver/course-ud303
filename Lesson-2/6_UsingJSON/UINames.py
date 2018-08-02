45dd58be344ccf3d7c801005f73cfbf2fe5ee725
Fatih İver <fatih.iver@boun.edu.tr>
2018-08-02 13:31:35 +0300

    Add files via upload

--- Lesson-2/6_UsingJSON/UINames.py
+++ Lesson-2/6_UsingJSON/UINames.py
@@ -15,10 +15,9 @@ def SampleRecord():
     r = requests.get("http://uinames.com/api?ext&region=United%20States",
                      timeout=2.0)
     # 1. Add a line of code here to decode JSON from the response.
+    json = r.json()
 
-    return "My name is {} {} and the PIN on my card is {}.".format(
-        # 2. Add the correct fields from the JSON data structure.
-    )
+    return "My name is {} {} and the PIN on my card is {}.".format(json["name"], json["surname"], json["credit_card"]["pin"])
 
 if __name__ == '__main__':
     print(SampleRecord())
